7ba835b0220c4dc84b0521aa578f2eeecd390d76
Nathanne Isip <nathanneisip@gmail.com>
2024-04-16 23:16:00 +0800

    Utility function for converting uint16 to [4]byte and vice versa.

--- util/conv.go
+++ util/conv.go
@@ -11,3 +11,13 @@ func BytesToFloat32(buf [4]byte) float32 {
 	bits := *(*uint32)(unsafe.Pointer(&buf))
 	return *(*float32)(unsafe.Pointer(&bits))
 }
+
+func Uint16ToBytes(u uint16) [4]byte {
+	bits := *(*[4]byte)(unsafe.Pointer(&u))
+	return bits
+}
+
+func BytesToUint16(buf [4]byte) uint16 {
+	bits := *(*uint32)(unsafe.Pointer(&buf))
+	return *(*uint16)(unsafe.Pointer(&bits))
+}
