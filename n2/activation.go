5699f7a95b6d3f47392a888667fa4e20aa44bfb6
Nathanne Isip <nathanneisip@gmail.com>
2024-04-16 22:25:07 +0800

    Improve parameter naming.

--- n2/activation.go
+++ n2/activation.go
@@ -2,7 +2,7 @@ package n2
 
 func sigmoidApprox(d float32) float32 {
 	result := 0.5 + (0.5 * d)
-	if result < 0 {
+	if result < 0.0 {
 		return 0.0
 	}
 
--- n2/feedforward.go
+++ n2/feedforward.go
@@ -1,60 +1,60 @@
 package n2
 
-func (nt *NeuralNetwork) Infer(input []float32) []float32 {
-	for c := uint8(0); c < nt.HiddenCount; c++ {
-		sum := nt.HiddenBias[c]
-		for r := uint8(0); r < nt.InputCount; r++ {
-			sum += input[r] * nt.HiddenWeights[r*nt.HiddenCount+c]
+func (neuralNet *NeuralNetwork) Infer(input []float32) []float32 {
+	for c := uint8(0); c < neuralNet.HiddenCount; c++ {
+		sum := neuralNet.HiddenBias[c]
+		for r := uint8(0); r < neuralNet.InputCount; r++ {
+			sum += input[r] * neuralNet.HiddenWeights[r*neuralNet.HiddenCount+c]
 		}
 
-		nt.HiddenNeuron[c] = sigmoidApprox(sum)
+		neuralNet.HiddenNeuron[c] = sigmoidApprox(sum)
 	}
 
-	for c := uint8(0); c < nt.OutputCount; c++ {
-		sum := nt.OutputBias[c]
-		for r := uint8(0); r < nt.HiddenCount; r++ {
-			sum += nt.HiddenNeuron[r] * nt.OutputWeights[r*nt.OutputCount+c]
+	for c := uint8(0); c < neuralNet.OutputCount; c++ {
+		sum := neuralNet.OutputBias[c]
+		for r := uint8(0); r < neuralNet.HiddenCount; r++ {
+			sum += neuralNet.HiddenNeuron[r] * neuralNet.OutputWeights[r*neuralNet.OutputCount+c]
 		}
 
-		nt.OutputNeuron[c] = sigmoidApprox(sum)
+		neuralNet.OutputNeuron[c] = sigmoidApprox(sum)
 	}
 
-	return nt.OutputNeuron
+	return neuralNet.OutputNeuron
 }
 
-func (nt *NeuralNetwork) Train(input []float32, y []float32, lr float32) {
-	nt.Infer(input)
+func (neuralNet *NeuralNetwork) Train(input []float32, y []float32, lr float32) {
+	neuralNet.Infer(input)
 
-	for c := uint8(0); c < nt.OutputCount; c++ {
-		nt.OutputGrad[c] = (nt.OutputNeuron[c] - y[c]) * 0.25
+	for c := uint8(0); c < neuralNet.OutputCount; c++ {
+		neuralNet.OutputGrad[c] = (neuralNet.OutputNeuron[c] - y[c]) * 0.25
 	}
 
-	for r := uint8(0); r < nt.HiddenCount; r++ {
-		nt.HiddenGrad[r] = 0
-		for c := uint8(0); c < nt.OutputCount; c++ {
-			nt.HiddenGrad[r] += nt.OutputGrad[c] * nt.OutputWeights[r*nt.OutputCount+c]
+	for r := uint8(0); r < neuralNet.HiddenCount; r++ {
+		neuralNet.HiddenGrad[r] = 0
+		for c := uint8(0); c < neuralNet.OutputCount; c++ {
+			neuralNet.HiddenGrad[r] += neuralNet.OutputGrad[c] * neuralNet.OutputWeights[r*neuralNet.OutputCount+c]
 		}
 
-		nt.HiddenGrad[r] *= 0.25
+		neuralNet.HiddenGrad[r] *= 0.25
 	}
 
-	for r := uint8(0); r < nt.HiddenCount; r++ {
-		for c := uint8(0); c < nt.OutputCount; c++ {
-			nt.OutputWeights[r*nt.OutputCount+c] -= lr * nt.OutputGrad[c] * nt.HiddenNeuron[r]
-		}
-	}
-
-	for r := uint8(0); r < nt.InputCount; r++ {
-		for c := uint8(0); c < nt.HiddenCount; c++ {
-			nt.HiddenWeights[r*nt.HiddenCount+c] -= lr * nt.HiddenGrad[c] * input[r]
+	for r := uint8(0); r < neuralNet.HiddenCount; r++ {
+		for c := uint8(0); c < neuralNet.OutputCount; c++ {
+			neuralNet.OutputWeights[r*neuralNet.OutputCount+c] -= lr * neuralNet.OutputGrad[c] * neuralNet.HiddenNeuron[r]
 		}
 	}
 
-	for c := uint8(0); c < nt.OutputCount; c++ {
-		nt.OutputBias[c] -= lr * nt.OutputGrad[c]
+	for r := uint8(0); r < neuralNet.InputCount; r++ {
+		for c := uint8(0); c < neuralNet.HiddenCount; c++ {
+			neuralNet.HiddenWeights[r*neuralNet.HiddenCount+c] -= lr * neuralNet.HiddenGrad[c] * input[r]
+		}
 	}
 
-	for c := uint8(0); c < nt.HiddenCount; c++ {
-		nt.HiddenBias[c] -= lr * nt.HiddenGrad[c]
+	for c := uint8(0); c < neuralNet.OutputCount; c++ {
+		neuralNet.OutputBias[c] -= lr * neuralNet.OutputGrad[c]
+	}
+
+	for c := uint8(0); c < neuralNet.HiddenCount; c++ {
+		neuralNet.HiddenBias[c] -= lr * neuralNet.HiddenGrad[c]
 	}
 }
