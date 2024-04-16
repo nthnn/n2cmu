b02979a52b1b50657fff095cc560e46fbddc067e
Nathanne Isip <nathanneisip@gmail.com>
2024-04-16 23:33:39 +0800

    UART read function for uin8 data type.

--- uart/reader.go
+++ uart/reader.go
@@ -21,8 +21,19 @@ func ReadUint16() (uint16, error) {
 	var buf []byte
 	count, readErr := machine.Serial.Read(buf)
 
-	if count == 4 && readErr != nil {
-		return util.BytesToUint16([4]byte(buf)), nil
+	if count == 2 && readErr != nil {
+		return util.BytesToUint16([2]byte(buf)), nil
+	}
+
+	return 0, readErr
+}
+
+func ReadUint8() (uint8, error) {
+	var buf []byte
+	count, readErr := machine.Serial.Read(buf)
+
+	if count == 1 && readErr != nil {
+		return util.BytesToUint8([1]byte(buf)), nil
 	}
 
 	return 0, readErr
