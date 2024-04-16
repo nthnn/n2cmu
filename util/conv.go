38122ea3953719848510829fbbc6852632f2835f
Nathanne Isip <nathanneisip@gmail.com>
2024-04-16 23:33:10 +0800

    Conversion functionalities for uint8 data type.

--- util/conv.go
+++ util/conv.go
@@ -12,12 +12,22 @@ func BytesToFloat32(buf [4]byte) float32 {
 	return *(*float32)(unsafe.Pointer(&bits))
 }
 
-func Uint16ToBytes(u uint16) [4]byte {
-	bits := *(*[4]byte)(unsafe.Pointer(&u))
+func Uint16ToBytes(u uint16) [2]byte {
+	bits := *(*[2]byte)(unsafe.Pointer(&u))
 	return bits
 }
 
-func BytesToUint16(buf [4]byte) uint16 {
+func BytesToUint16(buf [2]byte) uint16 {
 	bits := *(*uint32)(unsafe.Pointer(&buf))
 	return *(*uint16)(unsafe.Pointer(&bits))
+}
+
+func Uint8ToBytes(u uint8) [1]byte {
+	bits := *(*[1]byte)(unsafe.Pointer(&u))
+	return bits
+}
+
+func BytesToUint8(buf [1]byte) uint8 {
+	bits := *(*uint32)(unsafe.Pointer(&buf))
+	return *(*uint8)(unsafe.Pointer(&bits))
 }
